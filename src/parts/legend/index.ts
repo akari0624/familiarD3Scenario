f1d1706a3b5c90e0f072c3ef86c88bc8c5ed1a8e
akari0624 <akari0624@hotmail.com>
2019-05-30 21:26:08 +0800

    將legend的x, y改成用scale去算，修改中

--- src/parts/legend/index.ts
+++ src/parts/legend/index.ts
@@ -1,7 +1,9 @@
 import { select as D3Select, Selection, BaseType } from 'd3-selection'
-import { scaleOrdinal, ScaleOrdinal } from 'd3-scale'
+import { scaleOrdinal, ScaleOrdinal, scaleBand, ScaleBand, scaleLinear, ScaleLinear } from 'd3-scale'
+import { max } from 'd3-array'
 import { getClientRectWidthAndHeight } from '../utils'
-import { LengendDataType, LengendIconEnum } from '../../types'
+import { LengendDataType, LengendIconEnum, LegendPresentTypeEnum } from '../../types'
+
 
 export class Legend {
 
@@ -10,6 +12,8 @@ export class Legend {
   svgHeight: number
   d3ishSVG: Selection<SVGGElement, any, HTMLElement, any>
   colorScale: ScaleOrdinal<number, string>
+  presentType: LegendPresentTypeEnum
+  legendScale: ScaleBand<string> | ScaleLinear<number, number>
 
   constructor(svgDom: HTMLOrSVGElement) {
     this.svgDomRef = svgDom
@@ -17,7 +21,7 @@ export class Legend {
     const box = getClientRectWidthAndHeight(svgDom)
     this.svgWidth = box.width
     this.svgHeight = box.height
-    this.d3ishSVG = D3Select(svgDom as any)
+    this.initD3ishSVG()
 
     this.d3ishSVG.append('g').attr('class', 'legendWrapper')
   }
@@ -36,6 +40,30 @@ export class Legend {
       .attr('width', this.svgWidth)
       .attr('height', this.svgHeight)
       .attr('transform', `translate(0, 0)`)
+
+      this.portraitOrLandscape()
+  }
+
+  portraitOrLandscape() {
+
+    if (this.svgWidth <= this.svgHeight) {
+      this.presentType = LegendPresentTypeEnum.portrait
+    }else {
+      this.presentType = LegendPresentTypeEnum.landscape
+    }
+  }
+
+  prepareScale(data: LengendDataType[]) {
+
+    if (this.presentType === LegendPresentTypeEnum.portrait) {
+      this.legendScale = scaleBand()
+        .range([0, this.svgHeight])
+        .domain(data.map(d => d.text))
+    }else if(this.presentType === LegendPresentTypeEnum.landscape){
+      this.legendScale = scaleLinear()
+      .range([0, this.svgWidth])
+      .domain([0, max(data.map(d => d.text.length * 12))])  // 12 is font size
+    }
   }
 
   draw(data: LengendDataType[]) {
@@ -46,28 +74,57 @@ export class Legend {
     const dataBinds: Selection<BaseType, LengendDataType, SVGGElement, any>
      = this.d3ishSVG.select<SVGGElement>('.legendWrapper').selectAll('g').data(data)
 
-
+    this.prepareScale(data)
    const enterLegendWrapperGs = dataBinds.enter().append<SVGGElement>('g').attr('class', 'legendWrapper')
     const colorFunc = this.colorScale
+    const legendPresentType = this.presentType
+    const legendScaleBand = this.legendScale
     enterLegendWrapperGs.each(function (d, i) {
      
       const currG = D3Select<SVGGElement, LengendDataType>(this)
-      const currY = 30 * i
+      const rectInLegendWidth = 20
+      const rectInLegendHeight = 18
+if(legendPresentType === LegendPresentTypeEnum.portrait) {
+      const currYPortraitMode = 30 * i
       currG.append('rect')
-        .attr('width', 20)
-        .attr('height', 18)
+        .attr('width', rectInLegendWidth)
+        .attr('height', rectInLegendHeight)
         .attr('x', 0)
-        .attr('y', currY)
+        .attr('y', (legendScaleBand as ScaleBand<string>)(d.text))
         .style('fill', () => {
           return colorFunc(i);
         })
 
       currG.append('text')
         .attr('x', 25)
-        .attr('y', currY + 15)
+        .attr('y', (legendScaleBand as ScaleBand<string>)(d.text) + rectInLegendHeight - 4)
         .text(d => d.text)
         .style('text-anchor', 'start')
+      }else if (legendPresentType === LegendPresentTypeEnum.landscape) {
+        const estimatedTextWidth =  10 * d.text.length
+        const rectAndTextBetween = 5
+        const gLeftPadding = 5
+        const currRectX = (rectInLegendWidth + rectAndTextBetween + estimatedTextWidth + gLeftPadding) * i
+        const currTextX = (rectInLegendWidth + rectAndTextBetween) * (i + 1) + (estimatedTextWidth ) * i
+        currG.append('rect')
+          .attr('width', rectInLegendWidth)
+          .attr('height', rectInLegendHeight)
+          .attr('x', currRectX)
+          .attr('y', 3)
+          .style('fill', () => {
+            return colorFunc(i);
+          })
+  
+        currG.append('text')
+          .attr('x', currTextX)
+          .attr('y',  rectInLegendHeight)
+          .text(d => d.text)
+          .style('text-anchor', 'start')
+
+      }
+
     })
+  
 
   }
 
